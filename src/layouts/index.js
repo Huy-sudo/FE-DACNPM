157beb577a448bd11f91f1b1cc562ef42b9eb3e7
thanhxinhtraiuit <thanh147zz@gmail.com>
2021-05-31 12:14:29 +0700

    edit layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Header1 from './header'
-import { Affix, Button, Layout } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import { Affix, Button,Menu, Layout } from 'antd';
+import Profile from './profile'
 const { Header, Content, Footer, Sider } = Layout;
+
 class index extends Component {
     constructor(props) {
         super(props);
@@ -10,11 +13,25 @@ class index extends Component {
         return (
             <div>
                 <Layout>
-                    <Header className="header" style={{backgroundColor:'#092b00'}}>
-                        <Affix offsetTop={0} >
-                            <Header1 />
-                        </Affix>
+                <Affix offsetTop={0} >
+                        <Header className="header" style={{ backgroundColor: '#18cc30' }}>
+                            <div className="d-flex justify-content-between align-items-center w-100">
+                                <Menu style={{ backgroundColor: '#0ca625', height: '65px' }} mode="horizontal" defaultSelectedKeys={['2']} >
+                                    <Menu.Item key="mail" className="text-white " icon={<MailOutlined />}>
+                                        Phong Mach
+                                </Menu.Item>
+                                    <Menu.Item key="12312" className="text-white " icon={<MailOutlined />}>
+                                        Navigatioewewn One
+                                </Menu.Item >
+
+                                </Menu>
+                                <div>
+
+                                    <Profile></Profile>
+                                </div>
+                            </div>     
                     </Header>
+                    </Affix>
                 <Content style={{
                 width: '100%',
                 minHeight:'100vh'
--- src/layouts/profile.js
+++ src/layouts/profile.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import {Avatar} from 'antd'
+import { Menu, Dropdown, Space,Avatar } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from "react-router-dom";
+import { faSortDown, faUser, faLock, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 const profile = () => {
     return (
         <div>
-            <Avatar >USER</Avatar>    
+            <Dropdown 
+                trigger={['click']}
+                overlay={(
+                    <Menu>
+                        <Menu.Item key="0">
+                            <Link to={'/profile/dashboard'}><FontAwesomeIcon style={{width: 20}} icon={faUser}/> {('Profile')}</Link>
+                        </Menu.Item>
+                        <Menu.Item key="4">
+                            <Link to={ '/setting/user/change-password'}><FontAwesomeIcon style={{width: 20}} icon={faLock}/> {('Change password')}</Link>
+                        </Menu.Item>
+                        <Menu.Divider />
+                        <Menu.Item key="5">
+                            <a href={'/auth/logout'}><FontAwesomeIcon style={{width: 20}} icon={faSignOutAlt}/> {('Sign Out')}</a>
+                        </Menu.Item>
+                    </Menu>
+                )} 
+            >
+                <Space className="cursor-pointer">
+                    <Avatar >USER</Avatar>    
+                    <FontAwesomeIcon style={{fontSize: 20}} icon={faSortDown}/>
+                </Space>
+            </Dropdown> 
+            
         </div>
     );
 }
