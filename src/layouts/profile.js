4a73481f4fe50791650d717bda7e2e234956123b
Huy-sudo <hathuchuy321@gmail.com>
2021-12-06 09:39:25 +0700

    aa

--- src/layouts/profile.js
+++ src/layouts/profile.js
@@ -28,7 +28,6 @@ const profile = () => {
                     <FontAwesomeIcon style={{fontSize: 20}} icon={faSortDown}/>
                 </Space>
             </Dropdown> 
-            
         </div>
     );
 }
